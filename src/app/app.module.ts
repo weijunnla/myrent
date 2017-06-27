181170bbbf77edc5f9b7c5a52c5b0992d2c8f7d4
wzhang <wzhang@nla.gov.au>
2017-06-27 14:42:47 +1000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+// used to create fake backend
+import { fakeBackendProvider } from './helper/index';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+
+import { AuthGuard } from './guard/index';
+import { AuthenticationService, UserService } from './service/index';
+import { LoginComponent } from './login/index';
+import { HomeComponent } from './home/index';
+import { PropertyComponent } from './property/index';
+import { LeaseComponent } from './lease/index';
+import { PersonComponent } from './person/index';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        HomeComponent,
+        PropertyComponent,
+        LeaseComponent,
+        PersonComponent
+    ],
+    providers: [
+        AuthGuard,
+        AuthenticationService,
+        UserService,
+
+        // providers used to create fake backend
+        fakeBackendProvider,
+        MockBackend,
+        BaseRequestOptions
+    ],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
